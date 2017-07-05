3ab7a806f0d5f8795a84b24d8562142e45816d1b
mu-sross <sylvain.ross@marginalunit.com>
2017-07-05 14:26:18 -0500

    Attempt to use the mutation observer: almost there.

--- renderer.js
+++ renderer.js
@@ -3,113 +3,141 @@ window.$ = $
 const GoldenLayout = require('golden-layout');
 
 var myLayout = new GoldenLayout({
-	content:[{
+	content: [{
 		type: 'row',
-		content:[{
+		content: [{
 			type: 'component',
 			componentName: 'test-component'
-		},{
+		}, {
 			type: 'component',
 			componentName: 'test-component'
-		},{
+		}, {
 			type: 'component',
 			componentName: 'test-component'
-		},{
+		}, {
 			type: 'component',
 			componentName: 'test-component'
 		}]
 	}]
 });
 
-myLayout.on( 'stackCreated', function( stack ){
+myLayout.on('stackCreated', function (stack) {
 	stack
 		.header
 		.controlsContainer
-		.find( '.lm_close' ) //get the close icon
-		.off( 'click' ) //unbind the current click handler
-		.click(function(){
+		.find('.lm_close') //get the close icon
+		.off('click') //unbind the current click handler
+		.click(function () {
 			//add your own
-			if( confirm( 'really close this?' ) ) {
+			if (confirm('really close this?')) {
 				stack.remove();
 			}
 		});
 });
 
-myLayout.on( 'tabCreated', function( tab ){
+myLayout.on('tabCreated', function (tab) {
 	tab
 		.closeElement
-		.off( 'click' ) //unbind the current click handler
-		.click(function(){
+		.off('click') //unbind the current click handler
+		.click(function () {
 			//add your own
-			if( confirm( 'really close this?' ) ) {
+			if (confirm('really close this?')) {
 				tab.contentItem.remove();
 			}
 		});
 });
 
 function updatePosition(lmElement, webview) {
-      const element = $(lmElement);
-      let css = element.offset();
+	const element = $(lmElement);
+	let css = element.offset();
 
-      css['width'] = lmElement.width();
-      css['height'] = lmElement.height();
+	css['width'] = lmElement.width();
+	css['height'] = lmElement.height();
 
-      console.log(`updating webview "${webview.attr('id')}": width:${css.width}, height:${css.height}, top:${css.top}, left:${css.left}`);
+	const isVisible = lmElement.is(":visible");
 
-    	$(webview).css(css);
+	if (isVisible) {
+		$(webview).show();
+	} else {
+		$(webview).hide();
+	}
+	
+	// css['display'] = ;
+	// const isVisible = element.parentNode.style.display != 'none';
+	// css['display'] = element.parent().style;
+
+	console.log(`Updating webview "${webview.attr('id')}": display: ${isVisible}, width:${css.width}, height:${css.height}, top:${css.top}, left:${css.left}`);
+
+	$(webview).css(css);
 }
 
 let counter = 0;
 const colors = ['red', 'green', 'blue', 'pink', 'yellow', 'white', 'gray'];
 
-myLayout.registerComponent( 'test-component', function( container ) {
-  let webview = null;
-  let element = $(container.getElement());
+myLayout.registerComponent('test-component', function (container) {
+	let webview_id = counter++;
 
-  let webview_id = counter++;
+	let webview = null;
+	let element = $(container.getElement());
+	const containerId = `container-${webview_id}`;
 
-  container.on('open', function () {
+	element.attr('id', containerId)
+	
+	container.on('open', function () {
+		container.getElement().html('<div>Loading...</div>');
 
-	  container.getElement().html( '<div>Loading...</div>' );
+		webview = $('<webview src="./webview.html" nodeintegration ' +
+			'disablewebsecurity autosize></webview>');
 
-    webview = $('<webview src="./webview.html" nodeintegration ' +
-      'disablewebsecurity autosize></webview>');
+		webview.attr("id", "webview-" + webview_id);
+		webview.css({
+			"position": "absolute",
+			"background": colors[webview_id % colors.length]
+		});
 
-    webview.attr("id", "webview-" + webview_id);
-    webview.css({
-          "position": "absolute",
-          "background": colors[webview_id % colors.length]
-    });
+		$(".lm_root").append(webview);
 
-    $(".lm_root").append(webview)
+		updatePosition(element, webview);
 
-  });
+		const observer = new MutationObserver(function (mutations) {
+			mutations.forEach(function (mutation) {
+				console.log("Mutation detected: ", mutation, mutation.target.getAttribute('id'));
+				updatePosition(element, webview);
+			});
+		});
+		var config = { attributes: true, attributeFilter: ['style'] };
 
-  container.on('hide', function() {
-  	if (webview !== null) {
-      console.log(`hide fired for "${webview.attr('id')}"`);
-    	webview.hide();
-    }
-  });
+		const elementNode = document.getElementById(containerId);
+		observer.observe(elementNode.parentNode, config);
 
-  container.on('show', function() {
-  	if (webview !== null) {
-      console.log(`show fired for "${webview.attr('id')}"`);
-      updatePosition(element, webview);
+		webview.show();
 
-	    webview.show();
-    }
-  });
+	});
 
-  container.on('resize', function () {
-    if (webview !== null) {
-      updatePosition(element, webview);
-    }
-  });
+	// container.on('hide', function () {
+	// 	if (webview !== null) {
+	// 		console.log(`hide fired for "${webview.attr('id')}"`);
+	// 		// webview.hide();
+	// 	}
+	// });
 
-  container.on('close', function () {
-    alert('close');
-  });
+	// container.on('show', function () {
+	// 	if (webview !== null) {
+	// 		console.log(`show fired for "${webview.attr('id')}"`);
+	// 		// updatePosition(element, webview);
+	// 		// webview.show();
+	// 	}
+	// });
+
+	// container.on('resize', function () {
+	// 	if (webview !== null) {
+	// 		updatePosition(element, webview);
+	// 	}
+	// });
+
+	// container.on('close', function () {
+	// 	alert('close');
+	// });
 });
 
 myLayout.init();
